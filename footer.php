447b53e128fe85bcf7b969f6a1767822a76ec767
Risto Kurniawan <risto9f@gmail.com>
2019-03-19 13:50:56 +0700

    Penambahan Footer

---
+++ footer.php
@@ -0,0 +1,38 @@
+<div class="sticky-footer">
+    <div class="container">
+    <div class="row text-center">
+        <div class="col-md-4 footer">
+         <h5>Alamat Kampus</h5>
+             <ul class="anak"> 
+                 <li><i class="fa fa-map-marker" aria-hidden="true"></i> Jl. Raya Puspiptek Serpong, Tangerang Selatan - Banten</li>
+                 <li><i class="fa fa-phone" aria-hidden="true"></i>(021)7560542</li>
+                 <li><i class="fa fa-phone" aria-hidden="true"></i>(021) 756054</li>
+             </ul>
+        </div>
+        <div class="col-md-4 footer">
+             <h5>Fasilitas</h5>
+        <ul >
+                    <li>Fasilitas Akademik</li>
+                    <li>Fasilitas Kemahasiswaan</li>
+                    <li>Fasilitas Umum</li>
+                </ul>
+        </div>
+        <div class="col-md-4 footer">
+        <h5>Kemahasiswaan</h5>
+        <ul >
+                    <li>DPKM</li>
+                    <li>CDC</li>
+                    <li>Beasiswa</li>
+                    <li>Prestasi Mahasiswa</li>
+                    <li>Kalender Akademik</li>
+                    <li>Galeri</li>
+                </ul>
+        </div>
+        <div class="col-md-12 copyright">
+            <p>Hak Cipta &copy;Yes Man</p>
+        </div>
+           
+    </div>  
+      
+    </div>
+ </div>
